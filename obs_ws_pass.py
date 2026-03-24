0f49bb61dee61a28eb6fddf3905d25ad19f9fb1f
errornosignal <178248171+errornosignal@users.noreply.github.com>
2026-03-24 12:22:44 -0500

    Rename obs_ws_pass.py to src/obs_ws_pass.py

--- obs_ws_pass.py
+++
@@ -1,1 +0,0 @@
-password = "" # Set this to your OBS WebSocket password. Keep this file secure and do not share it.
---
+++ src/obs_ws_pass.py
@@ -0,0 +1,1 @@
+password = "" # Set this to your OBS WebSocket password. Keep this file secure and do not share it.
